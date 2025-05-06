08644b30e95399ac83dab9b0ddcb54a6dfd04c12
Anirban <anirbancodes@gmail.com>
2025-05-06 21:03:48 +0530

    arrays, objects, foreach, map, arrow functions

--- script.js
+++ script.js
@@ -93,3 +93,102 @@ Types of Datatypes:
 
 // console.log(calcFactAge2(2010));
 // console.log(calcFactAge(2035));
+
+// ARRAY //////////////////////////////////////////////////////////////
+
+// const facts = ["Delhi is capital of India", 2010, true];
+// // console.log(facts[4]);
+
+// const [text, createdIn, isCorrect] = facts;
+// // console.log(text);
+
+// const newFacts = [...facts, "society"];
+// console.log(newFacts);
+
+// OBJECTS ///////////////////////////////////////////////////////
+
+// const factObj = {
+//   text: "Delhi is capital of India",
+//   createdIn: 2010,
+//   isCorrect: true,
+//   createSummary: function () {
+//     return `The fact: ${this.text}. Created in ${this.createdIn}`;
+//   },
+// };
+
+// console.log(factObj.text);
+// console.log(factObj["isCorrect"]);
+
+// const { isCorrect, text } = factObj;
+
+// console.log(factObj.createSummary());
+
+// console.log(text);
+
+// const CATEGORIES = [
+//   { name: "technology", color: "#3b82f6" },
+//   { name: "science", color: "#16a34a" },
+//   { name: "finance", color: "#ef4444" },
+//   { name: "society", color: "#eab308" },
+//   { name: "entertainment", color: "#db2777" },
+//   { name: "health", color: "#14b8a6" },
+//   { name: "history", color: "#f97316" },
+//   { name: "news", color: "#8b5cf6" },
+// ];
+
+// [1, 2, 3, 4, 5].forEach(function (el) {
+//   //   console.log(el * 100);
+// });
+
+// const times10 = [1, 2, 3, 4, 5].map(function (el) {
+//   return el * 10;
+// });
+
+// const allCat = CATEGORIES.map((category) => category.name);
+
+// console.log(allCat);
+
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "Quantum computing poses a threat to current encryption methods, necessitating urgent updates to cryptographic practices.",
+//     source:
+//       "https://www2.deloitte.com/us/en/insights/focus/tech-trends.html?utm_source=chatgpt.com",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Potential signs of life detected on exoplanet K2-18b, 124 light-years away, via compounds DMS and DMDS found by JWST.",
+//     source:
+//       "https://www.thetimes.co.uk/article/were-in-a-golden-age-of-space-discovery-are-we-about-to-find-et-7bptzmltv?utm_source=chatgpt.com",
+//     category: "science",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "U.S. national debt reaches $158.6 trillion, equating to $974,000 per taxpayer, largely due to unaccounted obligations.",
+//     source:
+//       "https://www.truthinaccounting.org/news/detail/financial-state-of-the-union-2025?utm_source=chatgpt.com",
+//     category: "finance",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
+
+// const calcFactAge2 = (year) =>
+//   year <= new Date().getFullYear()
+//     ? new Date().getFullYear() - year
+//     : "This is impossible year";
+
+// const factAge = initialFacts.map((fact) => calcFactAge2(fact.createdIn));
+
+// console.log(factAge);
