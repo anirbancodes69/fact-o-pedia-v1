9e894112c090406ee5e3230246118b45af4ab616
Anirban <anirbancodes@gmail.com>
2025-05-12 04:10:53 +0530

    integrated backend api with our fromtend html css and js app

--- script.js
+++ script.js
@@ -192,3 +192,105 @@ Types of Datatypes:
 // const factAge = initialFacts.map((fact) => calcFactAge2(fact.createdIn));
 
 // console.log(factAge);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Quantum computing poses a threat to current encryption methods, necessitating urgent updates to cryptographic practices.",
+    source:
+      "https://www2.deloitte.com/us/en/insights/focus/tech-trends.html?utm_source=chatgpt.com",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Potential signs of life detected on exoplanet K2-18b, 124 light-years away, via compounds DMS and DMDS found by JWST.",
+    source:
+      "https://www.thetimes.co.uk/article/were-in-a-golden-age-of-space-discovery-are-we-about-to-find-et-7bptzmltv?utm_source=chatgpt.com",
+    category: "science",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "U.S. national debt reaches $158.6 trillion, equating to $974,000 per taxpayer, largely due to unaccounted obligations.",
+    source:
+      "https://www.truthinaccounting.org/news/detail/financial-state-of-the-union-2025?utm_source=chatgpt.com",
+    category: "finance",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const CATEGORIES = [
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "technology", color: "#3b82f6" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Creating html objects
+const btn = document.querySelector(".btn-share");
+const form = document.querySelector(".form");
+const fact = document.querySelector(".fact-list");
+
+// Creating the list elements
+fact.innerHTML = "";
+
+loadData();
+
+async function loadData() {
+  const res = await fetch("http://localhost:5000/api/facts");
+  const data = await res.json();
+  createFactList(data);
+}
+
+function createFactList(data) {
+  const dataArr = data.map(
+    (fact) => `<li class="fact"><p>
+                ${fact.text}
+                <a
+                  class="source"
+                  href="${fact.source}"
+                  target="_blank"
+                  >(Source)</a
+                >
+              </p>
+              <span class="tag" style="background-color: ${
+                CATEGORIES.find((cat) => cat.name === fact.category).color
+              }">${fact.category}</span></li>`
+  );
+
+  // console.log(dataArr);
+
+  console.log(CATEGORIES.find((cat) => cat.name === "technology").color);
+
+  const dataStr = dataArr.join("");
+  // console.log(dataStr);
+
+  fact.insertAdjacentHTML("afterbegin", dataStr);
+}
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+// console.log([1, 2, 37, 5, 35, 9].filter((el) => el < 5));
+// console.log([2, 1, 37, 5, 35, 9].find((el) => el < 5));
